bba72e983df992eb202d72cd91c9dec51df35457
leonardo_chips <leonardo_chips@hotmail.com>
2020-09-18 10:15:33 -0300

    First Commit

---
+++ BlazorApp1/BlazorApp1/Data/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+    }
+}
---
+++ BlazorApp1/BlazorApp1/Data/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class EmployeeService
+    {
+        private readonly AppDbContext _db;
+
+        public EmployeeService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        //FOR CRUD OPERATION
+        public List<Employee> GetEmployee()
+        {
+            var empList = _db.Employee.ToList();
+            return empList;
+        }
+
+        public string Create(Employee emp)
+        {
+            _db.Employee.Add(emp);
+            _db.SaveChanges();
+            return "Save Successfully";
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            Employee employee = _db.Employee.FirstOrDefault(s => s.EmployeeId == id);
+            return employee;
+        }
+
+        public string UpdateEmployee(Employee emp)
+        {
+            _db.Employee.Update(emp);
+            _db.SaveChanges();
+            return "Update Successfully";
+        }
+
+        public string DeleteEmployee(Employee emp)
+        {
+            _db.Employee.Remove(emp);
+            _db.SaveChanges();
+            return "Delete Successfully";
+        }
+    }
+}
